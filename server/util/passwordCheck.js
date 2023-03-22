18bcc2f9d3cb86848afbbe769c88fa7870ffd06d
Davy Kamanzi <davykamanzi@gmail.com>
2023-03-22 19:47:55 +0300

    Changing max length to 20 characters

--- server/util/passwordCheck.js
+++ server/util/passwordCheck.js
@@ -1,11 +1,11 @@
 /*
 Password must be:
-    - 8 to 15 characters long
+    - 8 to 20 characters long
     - contain at least one uppercase letter
     - contain at least one lowercase letter
     - contain at least one numeric digit
     - contain at least one special character
 */
 
-export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
 export const checkPassword = password => password.match(passwordRegex) ? true : false;
