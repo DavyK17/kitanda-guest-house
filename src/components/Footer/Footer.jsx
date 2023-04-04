904505a4e862f1a9fdb8ca7052bdd77a0b7ab7cc
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-04 18:20:10 +0300

    Initialising components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Demo app built by <a href="https://davyk17.github.io" target="_blank" rel="noreferrer">Davy Kamanzi</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/img/logos/logo.png";
+
+const Header = props => {
+    const { activeClassName } = props;
+
+    const iconMenu = (
+        <svg width="24" height="24" viewBox="0 0 24 24">
+            <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+        </svg>
+    )
+
+    const toggleMenu = e => {
+        e.preventDefault();
+        const nav = document.querySelector("header nav");
+        nav.classList.toggle("open");
+    }
+
+    return (
+        <header>
+            <img src={logo} width="200px" alt="Logo" />
+            <nav className="font-head-2">
+                <button onClick={toggleMenu}>{iconMenu}</button>
+                <ul>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : null} to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : null} to="/gallery">Gallery</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : null} to="/booking">Booking</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : null} to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
