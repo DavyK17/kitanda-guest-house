17ce8e6dd01a18160bbb9d69699af2089d7e12d1
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-04 21:56:13 +0300

    Adding NotFound page

--- src/components/Other/NotFound.jsx
+++ src/components/Other/NotFound.jsx
@@ -1,5 +1,14 @@
 const NotFound = () => {
-    return;
+    return (
+        <section id="error-404">
+            <div className="background white">
+                <div className="content align-items-start">
+                    <h1 className="bold font-head uppercase">404 Error</h1>
+                    <p>We're sorry, but the file you requested could not be found. It may have been removed, had its name changed, or is temporarily unavailable.</p>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default NotFound;
