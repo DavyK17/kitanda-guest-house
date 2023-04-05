d2353e0fbef05ea1aaf4d46d94c2a266ceee82b2
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-05 22:44:04 +0300

    Minor formatting

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import "./assets/style/reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/style/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router>
