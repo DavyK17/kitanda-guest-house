fb6313cd1184ad65873ab3b798bc42cdf9a165e0
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-05 23:28:11 +0300

    Moving Google Map to Home component

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,15 +1,14 @@
 const About = () => {
     return (
         <section id="about">
-            <div className="content">
-                <h1 className="bold font-head uppercase">Welcome to Kitanda Guest House</h1>
-                <p>Conveniently located along Kilimani Road off Elgeyo Marakwet Road, Kitanda Guest House is a great base from which to explore the vibrant city of Nairobi. The excitement of the city center is only 6.1 km away, and this modern establishment is in the vicinity of popular city attractions such as various shopping malls, Nairobi Club, the Kenya National Library and much more.</p>
-                <p>Kitanda Guest House offers impeccable service and all the essential amenities to invigorate travellers. While staying at this wonderful property, guests can enjoy free wi-fi in all rooms and public areas, car parking, room service, and airport transfer. The property has 32 rooms spread over 2 floors that provide a warm and pleasant home away from home.</p>
-                <p>Modern comforts such as wireless internet access (complimentary), wake-up services and desk can be found in selected rooms, and throughout the day you can enjoy the relaxing atmosphere of our garden. The property lies at the heart of good living district and surrounded by modern shopping malls like Greenhouse, Yaya Centre, Junction Mall, and Prestige Plaza amongst others. These are good for shopping places, eating cuisine such as Japanese, Chinese, Ethiopian, Continental and West African, and a variety of other entertainment to your liking. You can also arrange tours and excursions through the various firms nearby.</p>
-                <p>Whatever the purpose of your visit, Kitanda is an excellent choice for your stay in Nairobi.</p>
-            </div>
-            <div className="google-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7932552480142!2d36.7756095145725!3d-1.2988113990526837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1ba4ec8ef6a3%3A0x7662b0ae7a54a1bf!2sMalazi%20Bed%20%26%20Breakfast!5e0!3m2!1sen!2ske!4v1608029659814!5m2!1sen!2ske" title="Google map" width="100%" height="100%" frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+            <div className="background">
+                <div className="content">
+                    <h1 className="bold font-head uppercase">Welcome to Kitanda Guest House</h1>
+                    <p>Conveniently located along Kilimani Road off Elgeyo Marakwet Road, Kitanda Guest House is a great base from which to explore the vibrant city of Nairobi. The excitement of the city center is only 6.1 km away, and this modern establishment is in the vicinity of popular city attractions such as various shopping malls, Nairobi Club, the Kenya National Library and much more.</p>
+                    <p>Kitanda Guest House offers impeccable service and all the essential amenities to invigorate travellers. While staying at this wonderful property, guests can enjoy free wi-fi in all rooms and public areas, car parking, room service, and airport transfer. The property has 32 rooms spread over 2 floors that provide a warm and pleasant home away from home.</p>
+                    <p>Modern comforts such as wireless internet access (complimentary), wake-up services and desk can be found in selected rooms, and throughout the day you can enjoy the relaxing atmosphere of our garden. The property lies at the heart of good living district and surrounded by modern shopping malls like Greenhouse, Yaya Centre, Junction Mall, and Prestige Plaza amongst others. These are good for shopping places, eating cuisine such as Japanese, Chinese, Ethiopian, Continental and West African, and a variety of other entertainment to your liking. You can also arrange tours and excursions through the various firms nearby.</p>
+                    <p>Whatever the purpose of your visit, Kitanda is an excellent choice for your stay in Nairobi.</p>
+                </div>
             </div>
         </section>
     )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,9 @@ const Home = () => {
         <>
             <Top />
             <About />
+            <div className="google-map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7932552480142!2d36.7756095145725!3d-1.2988113990526837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1ba4ec8ef6a3%3A0x7662b0ae7a54a1bf!2sMalazi%20Bed%20%26%20Breakfast!5e0!3m2!1sen!2ske!4v1608029659814!5m2!1sen!2ske" title="Google map" width="100%" height="100%" frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+            </div>
             <Amenities />
             <Contact />
         </>
