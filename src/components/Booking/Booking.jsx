13f2b771599d0ab3f048c36c9b3fdde8805a00f8
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-06 21:01:23 +0300

    Updating component

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import CheckAvailability from "../Home/CheckAvailability";
+import RoomSelect from "./RoomSelect";
+
 const Booking = () => {
-    return;
+    const [step, setStep] = useState("booking");
+    const [cart, setCart] = useState([]);
+
+    const toggleCheckout = e => {
+        e.preventDefault();
+        setStep(step === "checkout" ? "booking" : "checkout");
+    }
+
+    const completeCheckout = e => {
+        e.preventDefault();
+        console.log("checkout");
+    }
+
+    return (
+        <>
+            <section id="booking-top">
+                <div className="background black">
+                    <h1 className="bold uppercase">Booking</h1>
+                    <p>Book your stay at our serene guest house today.</p>
+                </div>
+                <CheckAvailability />
+            </section>
+            <section id="booking-main">
+                {step === "checkout" ? <Checkout handleSubmit={completeCheckout} /> : <RoomSelect setCart={setCart} />}
+                <Cart cart={cart} step={step} handleCheckout={toggleCheckout} />
+            </section>
+        </>
+    )
 }
 
 export default Booking;
