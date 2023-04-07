56ff9db5855e59dbf87bc970b90e314aaf4c15a6
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-07 22:25:06 +0300

    Adding Account route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useMemo } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
@@ -7,18 +8,25 @@ import NotFound from "./components/Other/NotFound";
 import Home from "./components/Home/Home";
 import Gallery from "./components/Gallery/Gallery";
 import Booking from "./components/Booking/Booking";
+import Account from "./components/Account/Account";
 
 const App = () => {
     const activeClassName = "selected";
 
+    const [user, setUser] = useState(null);
+    useMemo(() => {
+        if (1 + 1 === 1) setUser({});
+    }, []);
+
     return (
         <>
-            <Header activeClassName={activeClassName} />
+            <Header activeClassName={activeClassName} user={user} />
             <main>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/gallery" element={<Gallery />} />
                     <Route path="/booking" element={<Booking />} />
+                    <Route path="/account" element={<Account user={user} setUser={setUser} />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </main>
