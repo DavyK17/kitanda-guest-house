92576a473e3b7dc8ef11f0ea90225ccf562e6a0a
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-10 13:37:24 +0300

    Adding Details component

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,9 +1,10 @@
 import Auth from "./Auth";
+import Details from "./Details";
 
 const Account = props => {
     const { user } = props;
 
-    return user ? null : <Auth />;
+    return user ? <Details user={user} /> : <Auth />;
 }
 
 export default Account;
