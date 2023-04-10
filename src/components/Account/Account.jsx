11d6a0a9f03a86a67216553686d63ec4602fe6df
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-10 14:51:10 +0300

    Passing setUser to components

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -2,9 +2,9 @@ import Auth from "./Auth";
 import Details from "./Details";
 
 const Account = props => {
-    const { user } = props;
+    const { user, setUser } = props;
 
-    return user ? <Details user={user} /> : <Auth />;
+    return user ? <Details user={user} setUser={setUser} /> : <Auth setUser={setUser} />;
 }
 
 export default Account;
