c3b380f8aff2b8ba84f5fe794d07ac463b49b6fd
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-15 03:10:20 +0300

    Passing dates to Booking component through search params

--- src/components/Home/Top.jsx
+++ src/components/Home/Top.jsx
@@ -1,17 +1,28 @@
+/* IMPORTS */
+import { useNavigate } from "react-router-dom";
+
 import CheckAvailability from "./CheckAvailability";
 import Slideshow from "./Slideshow";
 
+/* COMPONENT */
 const Top = () => {
-    const findRooms = e => {
+    // Define useNavigate()
+    let navigate = useNavigate()
+
+    // Define availability form submit function
+    const beginSearch = e => {
         e.preventDefault();
+        navigate(`/booking?checkInDate=${e.target[0].value}&checkOutDate=${e.target[1].value}`);
     }
 
+    // Return component
     return (
         <section id="top">
             <Slideshow />
-            <CheckAvailability handleSubmit={findRooms} />
+            <CheckAvailability handleSubmit={beginSearch} />
         </section>
     )
 }
 
+/* EXPORT */
 export default Top;
