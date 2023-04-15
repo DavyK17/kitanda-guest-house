89cb4e466501177321ec343e7854d3d5924f1d4e
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-16 00:05:45 +0300

    Fleshing out component

--- src/components/Booking/Cart.jsx
+++ src/components/Booking/Cart.jsx
@@ -1,14 +1,58 @@
 /* COMPONENT */
-// const Cart = props => {
-const Cart = () => {
+const Cart = props => {
     // Destructure props
-    // const { step, handleCheckout } = props;
-    
+    const { cart, setCart, step, handleCheckout } = props;
+
+    // Define function to render cart body
+    const renderCartBody = () => {
+        // Return message if cart is empty
+        if (cart.length === 0) return <p>No rooms have been selected.</p>;
+
+        // Define function to render cart total
+        const cartTotal = () => cart.map(item => item.pricePerNight).reduce((prev, curr) => prev + curr).toLocaleString("en-KE");
+
+        // Return cart body
+        return <>
+            {
+                cart.map(({ id, name, pricePerNight }, i) => {
+                    // Define function to remove item from cart
+                    const removeFromCart = async () => {
+                        const updated = [].concat(cart).filter(roomType => roomType.id !== id);
+                        setCart(updated);
+                    }
+
+                    // Return cart item
+                    return <div className="room-cart-single" key={i}>
+                        <div className="room-price">
+                            <div className="info-line">
+                                <p className="font-head-2 bold uppercase">Room:</p>
+                                <p className="font-head-2 uppercase">{name}</p>
+                            </div>
+                            <div className="info-line">
+                                <p className="font-head-2 bold uppercase">Price:</p>
+                                <p className="font-head-2 uppercase">Ksh. {pricePerNight.toLocaleString("en-KE")}</p>
+                            </div>
+                        </div>
+                        <div className="remove">
+                            <button className="font-head-2 uppercase" onClick={removeFromCart}>Remove</button>
+                        </div>
+                    </div>
+                })
+            }
+            <div className="room-cart-checkout">
+                <div className="info-line">
+                    <p className="font-head-2 bold uppercase">Total:</p>
+                    <p className="font-head-2 bold uppercase">Ksh. {cartTotal()}</p>
+                </div>
+                <button className="font-head-2 bold uppercase" onClick={handleCheckout}>{step === "checkout" ? "Back" : "Checkout"}</button>
+            </div>
+        </>
+    }
+
     // Return component
     return (
         <div className="cart" id="booking-cart">
             <div className="content">
-                {/* <!-- CART --> */}
                 <div className="room-cart">
                     <div className="room-cart-header">
                         <div className="info-line">
@@ -22,45 +66,9 @@ const Cart = () => {
                             <h3 className="font-head-2 light uppercase" id="check-out-date">DD-MM-YYYY</h3>
                         </div>
                     </div>
-    
+
                     <div className="room-cart-body">
-                        {/* <!-- TEXT TO SHOW WHEN CART IS EMPTY --> */}
-                        <p>No rooms have been selected.</p>
-    
-                        {/* <!-- Single booking --> */}
-                        {/* <div className="room-cart-single">
-                            <div className="info-line">
-                                <p className="font-head-2 bold uppercase">Room:</p>
-                                <p className="font-head-2 uppercase">Room type</p>
-                            </div>
-                            <div className="info-line">
-                                <p className="font-head-2 bold uppercase">Stay:</p>
-                                <div className="stay">
-                                    <p className="font-head-2 uppercase">N nights</p>
-                                    <p className="font-head-2 uppercase">(N adults, N children, N infants)</p>
-                                </div>
-                            </div>
-                            <div className="info-line">
-                                <p className="font-head-2 bold uppercase">Price:</p>
-                                <p className="font-head-2 uppercase">Ksh. 0,000</p>
-                            </div>
-                            <div className="remove">
-                                <button className="font-head-2 uppercase" href="#">Remove</button>
-                            </div>
-                        </div> */}
-    
-                        {/* <!-- Checkout button --> */}
-                        {/* <div className="room-cart-checkout">
-                            <div className="info-line">
-                                <p className="font-head-2 uppercase">Subtotal:</p>
-                                <p className="font-head-2 uppercase">Ksh. 0,000</p>
-                            </div>
-                            <div className="info-line">
-                                <p className="font-head-2 bold uppercase">Total:</p>
-                                <p className="font-head-2 bold uppercase">Ksh. 0,000</p>
-                            </div>
-                            <button className="font-head-2 bold uppercase" onClick={handleCheckout}>{step === "checkout" ? "Back" : "Checkout"}</button>
-                        </div> */}
+                        {renderCartBody()}
                     </div>
                 </div>
             </div>
