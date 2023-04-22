8b6dbca894e099d8d21f81f0fadd93d0b42708ab
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-23 01:29:34 +0300

    Adding checkout logic

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,18 +1,28 @@
 /* IMPORTS */
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Cart from "./Cart";
 import Checkout from "./Checkout";
 import CheckAvailability from "../Home/CheckAvailability";
 import RoomSelect from "./RoomSelect";
 
+import { getUser } from "../../api/Account";
+import { getAddresses } from "../../api/Addresses";
 import { getAvailableRoomTypes } from "../../api/RoomTypes";
+import { makeReservation } from "../../api/Reservations";
+import { createAddress } from "../../api/Addresses";
+
+import displayErrorMessage from "../../util/displayErrorMessage";
 
 /* COMPONENT */
 const Booking = props => {
     // Destructure props and dates
-    const { dates, setDates, cart, setCart, roomTypes, setRoomTypes, items, setItems } = props;
+    const { user, dates, setDates, cart, setCart, roomTypes, setRoomTypes, items, setItems } = props;
     const { checkInDate, checkOutDate } = dates;
+
+    // Define useNavigate()
+    let navigate = useNavigate();
 
     /* STATE + FUNCTIONS */
     // Loading
@@ -21,6 +31,34 @@ const Booking = props => {
 
     // Booking step state
     const [step, setStep] = useState("booking");
+
+    // Contacts
+    const [phone, setPhone] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [addresses, setAddresses] = useState([]);
+    const fetchContacts = async () => {
+        setIsLoading(true);
+
+        try {
+            let data = await getUser();
+            if (data) {
+                setPhone(data.phone);
+                setEmail(data.email);
+
+                data = await getAddresses();
+                if (data.length > 0) setAddresses(data);
+            }
+        } catch (err) {
+            console.error(err);
+            setError(true);
+        }
+
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        fetchContacts();
+    }, []);
 
     // Define function to automatically search for room types if search params are available
     const autoSearch = async () => {
@@ -56,9 +94,48 @@ const Booking = props => {
     }
 
     // Define function to complete checkout
-    const completeCheckout = e => {
+    const completeCheckout = async e => {
         e.preventDefault();
-        console.log("checkout");
+        let addressId;
+        const rooms = cart.map(({ id }) => id.toString());
+        const status = document.getElementById("status");
+
+        if (!user) {
+            const address1 = e.target[4].value;
+            const address2 = e.target[5].value;
+            const townCity = e.target[6].value;
+            const countyStateProvince = e.target[7].value;
+            const postcodeZip = e.target[8].value;
+            const country = e.target[9].value;
+
+            status.textContent = "Creating address…";
+            response = await createAddress(address1, address2, townCity, countyStateProvince, postcodeZip, country, reservationId);
+            if (!response.includes("Address created")) return displayErrorMessage(response);
+
+            addressId = response.split(" ").pop();
+        } else {
+            addressId = e.target[4].value;
+        }
+
+        status.textContent = "Making reservation…";
+        let response = await makeReservation(addressId, phone, checkInDate, checkOutDate, rooms, email);
+        if (!response.includes("Reservation made")) return displayErrorMessage(response);
+        const reservationId = response.split(" ").pop();
+
+        status.textContent = "Reservation made successfully.";
+        setTimeout(() => {
+            // Reset status element
+            status.textContent = null;
+
+            // Reset state
+            setDates({ checkInDate: null, checkOutDate: null });
+            setCart([]);
+            setRoomTypes();
+            setItems();
+
+            // Navigate to home page
+            navigate("/");
+        }, 3000);
     }
 
     // Define function to find available room types
@@ -97,7 +174,7 @@ const Booking = props => {
             <section id="booking-main">
                 {
                     step === "checkout" ?
-                        <Checkout handleSubmit={completeCheckout} /> :
+                        <Checkout user={user} phone={phone} setPhone={setPhone} email={email} setEmail={setEmail} addresses={addresses} handleSubmit={completeCheckout} /> :
                         <RoomSelect roomTypes={roomTypes} items={items} setItems={setItems} cart={cart} setCart={setCart} isLoading={isLoading} error={error} />
                 }
                 <Cart cart={cart} setCart={setCart} step={step} handleCheckout={toggleCheckout} />
