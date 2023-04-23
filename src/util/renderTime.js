e57603ce9c6cc1cf9f51c9f165e843e0b43981cd
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-23 14:49:00 +0300

    Adding date only option

--- src/util/renderTime.js
+++ src/util/renderTime.js
@@ -1,9 +1,11 @@
 // Function
-const renderTime = time => {
-    let options = { day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "2-digit", hour12: true };
-    let dateTime = new Intl.DateTimeFormat("en-KE", options);
-    return dateTime.format(new Date(time));
-}
+const renderTime = (time, dateOnly = false) => {
+	let options = { day: "numeric", month: "long", year: "numeric" };
+	if (!dateOnly) options = { ...options, hour: "numeric", minute: "2-digit", hour12: true };
+
+	let dateTime = new Intl.DateTimeFormat("en-KE", options);
+	return dateTime.format(new Date(time));
+};
 
 // Export
 export default renderTime;
