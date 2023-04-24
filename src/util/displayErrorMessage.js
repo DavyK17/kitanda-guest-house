3c619be012aaf9e9f2b07e68bcf8ad1d892dcbe1
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-25 00:17:53 +0300

    Adding message only option

--- src/util/displayErrorMessage.js
+++ src/util/displayErrorMessage.js
@@ -1,8 +1,10 @@
-const displayErrorMessage = (response) => {
+/* FUNCTION */
+const displayErrorMessage = (response, messageOnly = false) => {
 	const status = document.getElementById("status");
 
 	if (response.includes("undefined") || response === "An unknown error occurred. Kindly try again.") {
 		// Display generic error message
+		if (messageOnly) return "An unknown error occurred. Kindly try again.";
 		status.textContent = "An unknown error occurred. Kindly try again.";
 	} else {
 		// Remove "Error: " from error message
@@ -11,8 +13,10 @@ const displayErrorMessage = (response) => {
 		response = response.join(" ");
 
 		// Display error message
+		if (messageOnly) return response;
 		status.textContent = response;
 	}
 };
 
+/* EXPORT */
 export default displayErrorMessage;
