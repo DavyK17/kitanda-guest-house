ce8b9c7ddce755a3e4eb44d381df2461c6b02bae
Davy Kamanzi <davykamanzi@gmail.com>
2023-05-03 22:49:07 +0300

    Changing select names to IDs

--- src/components/Booking/RoomTypeSort.jsx
+++ src/components/Booking/RoomTypeSort.jsx
@@ -7,14 +7,14 @@ const RoomTypeSort = props => {
     return <div className="sort">
         <div>
             <label htmlFor="sort-by">Sort by</label>
-            <select name="sort-by" onChange={handleChangeSort}>
+            <select id="sort-by" onChange={handleChangeSort}>
                 <option value="ascending">Price (low to high)</option>
                 <option value="descending">Price (high to low)</option>
             </select>
         </div>
         <div>
             <label htmlFor="show-price">Show price</label>
-            <select name="show-price" onChange={handleChangePriceType}>
+            <select id="show-price" onChange={handleChangePriceType}>
                 <option value="per-night">Per night</option>
                 <option value="total">Total</option>
             </select>
