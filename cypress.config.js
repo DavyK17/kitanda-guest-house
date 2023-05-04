16653cd588327928de0cc6d335915034eb4a89af
Davy Kamanzi <davykamanzi@gmail.com>
2023-05-04 23:50:59 +0300

    Minor adjustments

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ import { defineConfig } from "cypress";
 export default defineConfig({
 	projectId: "dav9p6",
 	e2e: {
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
+		// setupNodeEvents(on, config) {
+		// 	// implement node event listeners here
+		// },
 		baseUrl: "http://localhost:8000",
-		specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
 		experimentalStudio: true,
+		specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
 		video: false,
 	},
 });
